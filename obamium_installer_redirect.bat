8902ce5a3277a5e26604ad28c0ff7cc85774cb03
Timctro <69670311+Timctro@users.noreply.github.com>
2021-10-08 17:12:00 +1100

    Current Source for OBAMIUM, not including personal edits.

---
+++ obamium_installer_redirect.bat
@@ -0,0 +1,137 @@
+@echo off
+color a
+cls
+echo This only works for windows.
+pause
+cls
+echo              _ _,---._ 
+PING localhost -n 2 >NUL
+echo           ,-','       `-.___ 
+PING localhost -n 2 >NUL
+echo          /-;'               `._ 
+PING localhost -n 2 >NUL
+echo         /\/          ._   _,'o \ 
+PING localhost -n 2 >NUL
+echo        ( /\       _,--'\,','"`. ) 
+PING localhost -n 2 >NUL
+echo         ^|\      ,'o     \'    //\ 
+PING localhost -n 2 >NUL
+echo         ^|      \        /   ,--'""`-. 
+PING localhost -n 2 >NUL
+echo         :       \_    _/ ,-'         `-._ 
+PING localhost -n 2 >NUL
+echo          \        `--'  /                ) 
+PING localhost -n 2 >NUL
+echo           `.  \`._    ,'     ________,',' 
+PING localhost -n 2 >NUL
+echo             .--`     ,'  ,--` __\___,;' 
+PING localhost -n 2 >NUL
+echo              \`.,-- ,' ,`_)--'  /`.,' 
+echo               \^( ;  ^| ^| )      ^(`-/ 
+echo                 `--'^| ^|^)       ^|-/ 
+echo                   ^| ^| ^|        ^| ^| 
+echo                   ^| ^| ^|,.,-.   ^| ^|_ 
+echo                   ^| `./ /   )---`  ) 
+echo                  _^|  /    ,',   ,-' 
+echo                 ,'^|_(    /-^<._,' ^|--, 
+echo                 ^|    `--'---.     \/ \ 
+echo                 ^|          / \    /\  \ 
+echo               ,-^---._     ^|  \  /  \  \ 
+echo            ,-'        \----'   \/    \--`. 
+echo           /            \              \   \ 
+pause
+echo Welcome to the batch file version of the Obamium Discord Bot's Source
+echo Made by Timctro and George W. Bush. Copyright 2021 Timctro. Non-distributable
+echo bit.ly/bush420 bit.ly/dscplus bit.ly/dscplus
+echo.
+echo Please wait until a message appears
+PING localhost -n 4 >NUL
+pause
+cls
+echo Starting installation.
+PING localhost -n 2 >NUL
+cls
+echo Starting installation..
+PING localhost -n 2 >NUL
+cls
+echo Starting installation...
+PING localhost -n 2 >NUL
+cls
+echo Starting installation.
+PING localhost -n 2 >NUL
+cls
+echo Starting installation..
+PING localhost -n 2 >NUL
+cls
+echo Starting installation...
+PING localhost -n 2 >NUL
+pause
+echo Refer to the GitHub README.md as a guide.
+start https://bit.ly/3AyQfyh
+echo https://bit.ly/3AyQfyh
+pause
+cls
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+echo still here?
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+pause
+echo nah bro
+pause
+cls
+echo cya!
+pause
+cls
+end
